1759da083ee2df7f4073e053b821cbc66a1c1d8d
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-03-05 20:41:02 +0530

    commit

---
+++ Searching an element in a sorted array.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    {
+        int s=0;
+        int e=N-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(arr[mid]==K){
+                return 1;
+            }
+            else if(arr[mid]>K){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.searchInSorted(arr, n, k) << endl;
+
+    }
+
+	return 0; 
+} 
+
+// } Driver Code Ends
