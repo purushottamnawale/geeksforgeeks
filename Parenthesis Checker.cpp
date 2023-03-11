4ba911ddc6f1c4348a960a79b4880c2149c8b478
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-03-11 17:41:02 +0530

    commit

---
+++ Parenthesis Checker.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    // Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        stack<char> s;
+        for (int i = 0; i < x.size(); i++)
+        {
+            if (x[i] == '(' || x[i] == '[' || x[i] == '{')
+            {
+                s.push(x[i]);
+            }
+            else if (x[i] == ')')
+            {
+                if (!s.empty() && s.top() == '(')
+                {
+                    s.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (x[i] == ']')
+            {
+                if (!s.empty() && s.top() == '[')
+                {
+                    s.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (x[i] == '}')
+            {
+                if (!s.empty() && s.top() == '{')
+                {
+                    s.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return s.empty();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    string a;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a;
+        Solution obj;
+        if (obj.ispar(a))
+            cout << "balanced" << endl;
+        else
+            cout << "not balanced" << endl;
+    }
+}
+// } Driver Code Ends
