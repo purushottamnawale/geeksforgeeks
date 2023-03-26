64c1beda5ad9b95ac3b86855148e9df41f1515ff
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-03-26 15:44:27 +0530

    commit

---
+++ Frogs and Jumps.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+   int unvisitedLeaves(int N, int leaves, int frogs[]) {
+        // Code here
+
+    std::bitset<1000000> visited;
+    for (int i = 0; i < N; i++) {
+        int num = frogs[i];
+        if (num <= leaves && !visited[num]) {
+            for (int j = num; j <= leaves; j += num) {
+                visited.set(j);
+            }
+        }
+    }
+    int unvisited =leaves - visited.count();
+  
+    return unvisited;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, leaves;
+        cin >> N >> leaves;
+        int frogs[N];
+        for (int i = 0; i < N; i++) {
+            cin >> frogs[i];
+        }
+
+        Solution ob;
+        cout << ob.unvisitedLeaves(N, leaves, frogs) << endl;
+    }
+}
+// } Driver Code Ends
