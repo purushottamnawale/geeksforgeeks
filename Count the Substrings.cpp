dfdc42af88cf4b6d4513f37dbea34d18cfb25bc7
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-03-30 22:03:05 +0530

    commit

--- Count the Substrings.cpp
+++ Count the Substrings.cpp
@@ -5,6 +5,20 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+
+/*
+    For input 1 WomensDAY
+    1. Replace each uppercase character with 1 and lower case character with -1.
+    0  1 -1 -1 -1 -1 -1  1  1  1
+    Any subarray having sum 0 will be having equal number of upper case and lower case characters.
+    After the map declaration, we store zero with count 1;
+
+    2. Use map and prefix sum to count the number of subarrays having zero sum.
+    0  1  0 -1 -2 -3 -4 -3 -2 -1
+
+    3. In above prefix sum, whenever a sum repeats itself, it means that there is a zero sum subarray.
+*/
+
 class Solution
 {
     public:
@@ -23,7 +37,7 @@ class Solution
                 val=-1;
             }
             sum+=val;
-            if(map.find(sum)!=map.end()){
+            if(map.find(sum)!=map.end()){ // If map.find(sum) != map.end(), it means that the key sum is found in the map, and there is at least one element with that key. If map.find(sum) == map.end(), it means that the key sum is not found in the map, and there are no elements with that key.
                 ans+=map[sum];
             }
             map[sum]++;
