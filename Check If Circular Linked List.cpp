72602aa891c5cbe351583f7401166f58b8805fe7
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-04-01 22:02:32 +0530

    commit

--- Check If Circular Linked List.cpp
+++ Check If Circular Linked List.cpp
@@ -2,22 +2,21 @@
 // C program to find n'th Node in linked list
 #include <stdio.h>
 #include <stdlib.h>
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 /* Link list Node */
 struct Node
 {
     int data;
-    struct Node* next;
-    
-    Node(int x){
+    struct Node *next;
+
+    Node(int x)
+    {
         data = x;
         next = NULL;
     }
-    
 };
-
 
 /* Function to get the middle of the linked list*/
 bool isCircular(struct Node *head);
@@ -25,60 +24,60 @@ bool isCircular(struct Node *head);
 /* Driver program to test above function*/
 int main()
 {
-  int T,i,n,l,k;
+    int T, i, n, l, k;
 
-    cin>>T;
+    cin >> T;
 
-    while(T--){
+    while (T--)
+    {
 
-        cin>>n>>k;
-	    Node *head=NULL, *tail = NULL;
-	    int x;
-	    cin >> x;
-	    head = new Node(x);
-	    tail = head;
-	    for(int i=0;i<n-1;i++)
-	    {
-	        cin>>x;
-	        tail -> next = new Node(x);
-	        tail = tail -> next;
-	    }
-        if (k==1 && n >= 1)
-              tail->next = head;
+        cin >> n >> k;
+        Node *head = NULL, *tail = NULL;
+        int x;
+        cin >> x;
+        head = new Node(x);
+        tail = head;
+        for (int i = 0; i < n - 1; i++)
+        {
+            cin >> x;
+            tail->next = new Node(x);
+            tail = tail->next;
+        }
+        if (k == 1 && n >= 1)
+            tail->next = head;
 
-
-    printf("%d\n", isCircular(head));
+        printf("%d\n", isCircular(head));
     }
     return 0;
 }
 
-
 // } Driver Code Ends
 
-
-/* Link list Node 
+/* Link list Node
 struct Node
 {
     int data;
     struct Node* next;
-    
+
     Node(int x){
         data = x;
         next = NULL;
     }
-    
-}; 
+
+};
 */
 
 /* Should return true if linked list is circular, else false */
 bool isCircular(Node *head)
 {
-    Node *temp=head;
-    while(temp!=NULL){
-        if(temp->next==head){
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->next == head)
+        {
             return 1;
         }
-            temp=temp->next;
+        temp = temp->next;
     }
     return 0;
 }
