2a5252ac7327b62a8dff15822ba0f0c1f5c47b1b
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-04-03 21:12:23 +0530

    commit

---
+++ Remainder on dividing by 11.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+
+public:
+    int xmod11(string x)
+    {
+       long long int y=int(x);
+       return y%11;
+    
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string x;
+        cin>>x;
+        Solution ob;
+        int ans = ob.xmod11(x);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
