51a037a5e736eb15e5d7382462a2ce7ace0fdf69
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-04-13 21:24:55 +0530

    commit

---
+++ Pattern 14.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    void printTriangle(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                cout << char('A' + j);
+            }
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
