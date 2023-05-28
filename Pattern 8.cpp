801df324a5907bf3a0a12493a40b5a4066667a00
Purushottam Nawale <nawalepurushottam@gmail.com>
2023-05-28 16:10:16 +0530

    commit

---
+++ Pattern 8.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+/*
+  N = 5
+  0 1 2 3 4 5 6 7 8
+
+0 * * * * * * * * *
+1   * * * * * * *
+2     * * * * *
+3       * * *
+4         *
+
+*/
+
+class Solution
+{
+public:
+    void printTriangle(int n)
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < n + i; j++)
+            {
+                if (j >= (n - 1 - i))
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
