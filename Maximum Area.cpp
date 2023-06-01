d18829282da12e0ffe695b5f2837cbb56e792281
Purushottam Nawale <nawalepurushottam@gmail.com>
2023-06-01 20:33:00 +0530

    commit

---
+++ Maximum Area.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int getHypotenuse(long long N)
+    {
+        return 2 * sqrt(N);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+
+        cin >> N;
+
+        Solution ob;
+        cout << ob.getHypotenuse(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
