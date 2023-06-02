51df9cfbb74610827c77ecc7ecc15eb0b805abdf
Purushottam Nawale <nawalepurushottam@gmail.com>
2023-06-02 14:14:26 +0530

    commit

--- Maximum Area.cpp
+++ Maximum Area.cpp
@@ -5,7 +5,26 @@ using namespace std;
 // } Driver Code Ends
 
 /*
+Area of Triangle = 1/2 * Base * Height
 
+The area of a right-angled triangle of the given hypotenuse is maximum when the triangle is isosceles.
+i.e. Base = Height
+
+Area = 1/2 * Base * Height
+2.Area = Base*Height
+2.Area = Base^2                         1)
+
+
+By Pythagoras theorem, we know that
+Hypotenuse^2 = Base^2 + Height^2
+
+Hypotenuse^2 = 2.Base^2                2)
+
+From 1) and 2)
+Hypotenuse^2 = 2.Area
+Hypotenuse = sqrt(2.Area)
+
+return 2 * sqrt(N);
 */
 class Solution
 {
