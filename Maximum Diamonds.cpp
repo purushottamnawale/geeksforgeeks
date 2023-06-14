b1b1d27a12d4605e8bc49710c3932f340dbaad14
purushottamnawale <nawalepurushottam@gmail.com>
2023-06-14 23:49:27 +0530

    commit

---
+++ Maximum Diamonds.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    long long maxDiamonds(int A[], int N, int K)
+    {
+        priority_queue<int> pq(A, A + N);
+        long long int ans = 0;
+        while (K--)
+        {
+            ans += pq.top();
+            pq.push(pq.top() / 2);
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, K;
+
+        cin >> N >> K;
+        int A[N];
+
+        for (int i = 0; i < N; i++)
+            cin >> A[i];
+
+        Solution ob;
+        cout << ob.maxDiamonds(A, N, K) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
