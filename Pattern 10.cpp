9d1b9466fea6d922017749c0b7bb7377a3e657f5
purushottamnawale <nawalepurushottam@gmail.com>
2023-06-19 21:35:39 +0530

    commit

---
+++ Pattern 10.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+// Back-end complete function Template for C++
+class Solution
+{
+public:
+    void printTriangle(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+        for (int i = n - 1; i > 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
