bae4a9126358dbbf4b905e88188c3967526e2bf6
purushottamnawale <nawalepurushottam@gmail.com>
2023-06-21 22:25:48 +0530

    commit

---
+++ Reverse Coding.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int sumOfNaturals(int n)
+    {
+        long int mod = 1e9 + 7;
+
+        return (long(n) * (long(n) + 1) / 2) % mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+
+        cin >> n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
