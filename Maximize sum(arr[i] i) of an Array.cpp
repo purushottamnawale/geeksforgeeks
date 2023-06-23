89dc96773381f19bad060d1a45ab3662f2d9c0dc
purushottamnawale <nawalepurushottam@gmail.com>
2023-06-23 23:39:56 +0530

    commit

---
+++ Maximize sum(arr[i] i) of an Array.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int Maximize(int a[], int n)
+    {
+        long long int sum = 0, mod = 1e9 + 7;
+        sort(a, a + n);
+        for (long long int i = 0; i < n; i++)
+        {
+            sum += (a[i] * i) % mod;
+        }
+        return sum % mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution ob;
+        cout << ob.Maximize(a, n) << endl;
+    }
+}
+// } Driver Code Ends
