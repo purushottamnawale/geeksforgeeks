c1b58218d46eaad50a416233fc44b21ca2fee6e7
purushottamnawale <nawalepurushottam@gmail.com>
2023-07-08 20:19:19 +0530

    commit

---
+++ Unique Number of Occurrences.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    bool isFrequencyUnique(int n, int arr[])
+    {
+        unordered_map<int, int> Occurence;
+        for (int i = 0; i < n; i++)
+        {
+            Occurence[arr[i]]++;
+        }
+        unordered_set<int> uniqueOccurence;
+        for (auto &i : Occurence)
+        {
+            if (uniqueOccurence.count(i.second))
+            {
+                return false;
+            }
+            else
+            {
+                uniqueOccurence.insert(i.second);
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        Solution ob;
+        bool ans = ob.isFrequencyUnique(n, arr);
+        if (ans)
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+}
+// } Driver Code Ends
