110e0516268c7782d754fdf1d4aa70a1828c22a5
purushottamnawale <nawalepurushottam@gmail.com>
2023-07-28 23:41:32 +0530

    commit

--- Level order traversal in spiral form.cpp
+++ Level order traversal in spiral form.cpp
@@ -126,47 +126,119 @@ struct Node
 }; */
 
 // Function to return a list containing the level order traversal in spiral form.
-// vector<int> findSpiral(Node *root)
-// {
-//     vector<int> ans;
-//     if (root == NULL)
-//     {
-//         return ans;
-//     }
-//     bool flag = false;
-//     queue<Node *> q;
-//     q.push(root);
-//     while (q.size()) // while queue is not empty
-//     {
-//         int n = q.size(); // number of nodes in current level
-//         stack<int> s;
-//         for (int i = 0; i < n; i++) // for each level
-//         {
-//             Node *temp = q.front();
-//             q.pop();
-//             if (flag == true)
-//             {
-//                 ans.push_back(temp->data); // if traversal is from left to right, then push the element in ans
-//             }
-//             else
-//             {
-//                 s.push(temp->data); // if traversal is from right to left, then push the element in stack
-//             }
-//             if (temp->left != NULL)
-//             {
-//                 q.push(temp->left);
-//             }
-//             if (temp->right != NULL)
-//             {
-//                 q.push(temp->right);
-//             }
-//         }
-//         while (s.size()) // while stack has some elements
-//         {
-//             ans.push_back(s.top());
-//             s.pop();
-//         }
-//         flag = !flag; // change the direction of traversal
-//     }
-//     return ans;
-// }
+vector<int> findSpiral(Node *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+    {
+        return ans;
+    }
+    bool flag = false;
+    queue<Node *> q;
+    q.push(root);
+    while (q.size()) // while queue is not empty
+    {
+        int n = q.size(); // number of nodes in current level
+        stack<int> s;
+        for (int i = 0; i < n; i++) // for each level
+        {
+            Node *temp = q.front();
+            q.pop();
+            if (flag == true)
+            {
+                ans.push_back(temp->data); // if traversal is from left to right, then push the element in ans
+            }
+            else
+            {
+                s.push(temp->data); // if traversal is from right to left, then push the element in stack
+            }
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+        }
+        while (s.size()) // while stack has some elements
+        {
+            ans.push_back(s.top());
+            s.pop();
+        }
+        flag = !flag; // change the direction of traversal
+    }
+    return ans;
+}
+
+/*
+Another Approach: Recursive
+
+int height(struct Node *root)
+{
+    // base case for recursion.
+    if (root == NULL)
+        return 0;
+
+    // finding height of left subtree recursively.
+    int l = 1 + height(root->left);
+
+    // finding height of right subtree recursively.
+    int r = 1 + height(root->right);
+
+    // taking the maximum as height of tree.
+    if (l > r)
+        return l;
+    else
+        return r;
+}
+
+// Function to do level order traversal of tree in spiral form.
+void spiral(vector<int> &ans, struct Node *root, int level, int flag)
+{
+    if (root == NULL) // base case for recursion
+        return;
+
+    if (level == 1) // if level is 1, we push the data in the ans vector
+    {
+        ans.push_back(root->data);
+    }
+
+    else if (level > 1)
+    {
+        if (flag) // if flag is true, we call the spiral function recursively for the left subtrees first and then for right subtrees.
+        {
+            spiral(ans, root->left, level - 1, flag);
+            spiral(ans, root->right, level - 1, flag);
+        }
+
+        else // else we call for right subtrees first and then left subtrees.
+        {
+            spiral(ans, root->right, level - 1, flag);
+            spiral(ans, root->left, level - 1, flag);
+        }
+    }
+}
+
+// Function to return a list containing the level order traversal in spiral form.
+vector<int> findSpiral(struct Node *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+    {
+        return ans;
+    }
+
+    int h = height(root);
+    bool flag = false;
+
+    for (int i = 1; i <= h; i++) // for each level, we do the spiral traversal
+    {
+        spiral(ans, root, i, flag);
+        flag = !flag;
+    }
+
+    return ans;
+}
+
+*/
