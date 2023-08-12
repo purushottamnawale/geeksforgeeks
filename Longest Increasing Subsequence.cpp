af40102006eb5adfe4c531f39c13297ef1a35c0d
purushottamnawale <nawalepurushottam@gmail.com>
2023-08-12 22:39:28 +0530

    Longest Increasing Subsequence

---
+++ Longest Increasing Subsequence.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    // Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[])
+    {
+        vector<int> res;
+        for (int i = 0; i < n; i++)
+        {
+            auto it = lower_bound(res.begin(), res.end(), a[i]);
+            if (it == res.end())
+            {
+                res.push_back(a[i]);
+            }
+            else
+            {
+                *it = a[i];
+            }
+        }
+        return res.size();
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    // taking total testcases
+    int t, n;
+    cin >> t;
+    while (t--)
+    {
+        // taking size of array
+        cin >> n;
+        int a[n];
+
+        // inserting elements to the array
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution ob;
+        // calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
