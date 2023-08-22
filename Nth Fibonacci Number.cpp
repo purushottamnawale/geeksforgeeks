d617abe64aa529917db4d02f8dd78ca02cd9208f
purushottamnawale <nawalepurushottam@gmail.com>
2023-08-22 22:57:55 +0530

    Nth Fibonacci Number

--- Nth Fibonacci Number.cpp
+++ Nth Fibonacci Number.cpp
@@ -7,7 +7,11 @@ using namespace std;
 // User function Template for C++
 
 /*
-Nth Fibonacci Number:
+Nth Fibonacci Number
+
+Fibonacci Numbers:
+0  1  2  3  4  5  6   7   8   9  10  11   12   13
+0  1  1  2  3  5  8  13  21  34  55  89  144  233
 Input:
 1 2
 
