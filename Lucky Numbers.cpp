7c279ee9b0b0a7013da02039ae73e6d76d11321f
purushottamnawale <nawalepurushottam@gmail.com>
2023-09-17 21:13:53 +0530

    Lucky Numbers

--- Lucky Numbers.cpp
+++ Lucky Numbers.cpp
@@ -1,45 +1,89 @@
 //{ Driver Code Starts
-//Initial Template for C++
+// Initial Template for C
 
-
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <stdbool.h>
 
 // } Driver Code Ends
-//User-function template for C++
+// User function Template for C
 
 // Complete the function
 // n: Input n
-// Return True if the given number is a lucky number else return False
+// Return true if the given number is a lucky number else return False
 
-class Solution{
-public:
-bool isLucky(int n) {
-   // code here
-   for(int i=2;i<=n;i++)
-   {
-       if(n%i==0)
-       return false;
-       n-=n/i;
-   }
-   return true; 
+/*
+Lucky Numbers
+
+Input:
+1 19
+
+Output:
+1
+
+
+TC : O(n)
+Explanation:
+n = 19
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+
+First Iteration, i = 2
+n = n - n/2
+n = 19 - 19/2
+n = 10
+1 3 5 7 9 11 13 15 17 19
+
+Second Iteration, i = 3
+n = n - n/3
+n = 10 - 10/3
+n = 7
+1 3 7 9 13 15 19
+
+Third Iteration, i = 4
+n = n - n/4
+n = 7 - 7/4
+n = 6
+1 3 7 13 15 19
+
+Fourth Iteration, i = 5
+n = n - n/5
+n = 6 - 6/5
+n = 5
+1 3 7 13 19
+
+Since, i > n, i.e. 6 > 5
+Loop breaks
+
+So, 19 is a Lucky Number
+*/
+
+bool isLucky(int n)
+{
+    for (int i = 2; i <= n; i++)
+    {
+        if (n % i == 0) // if n is divisible by i, then it is not a lucky number
+        {
+            return false;
+        }
+        n -= n / i;
+    }
+    return true;
 }
-};
 
 //{ Driver Code Starts.
-signed main(){
+
+int main()
+{
     int T;
-    cin>>T;
-    while(T--){
+    scanf("%d", &T);
+    while (T--)
+    {
         int n;
-        cin>>n;
-        Solution obj;
-        //calling isLucky() function
-        if(obj.isLucky(n))
-            cout<<"1\n";//printing "1" if isLucky() returns true
+        scanf("%d", &n);
+        // calling isLucky() function
+        if (isLucky(n))
+            printf("1\n"); // printing "1" if isLucky() returns true
         else
-            cout<<"0\n";//printing "0" if isLucky() returns false
+            printf("0\n"); // printing "0" if isLucky() returns false
     }
-    
 }
 // } Driver Code Ends
