7d861d44273e6dca549e50452448f95bbbfe438d
purushottamnawale <nawalepurushottam@gmail.com>
2023-09-17 21:13:30 +0530

    Print first n Fibonacci Numbers

--- Print first n Fibonacci Numbers.cpp
+++ Print first n Fibonacci Numbers.cpp
@@ -7,9 +7,39 @@ using namespace std;
 // } Driver Code Ends
 // User function template for C++
 
+/*
+Print first n Fibonacci Numbers
+
+Fibonacci Numbers:
+0  1  2  3  4  5  6   7   8   9  10  11   12   13
+0  1  1  2  3  5  8  13  21  34  55  89  144  233
+
+Input:
+1 11
+
+Output:
+1 1 2 3 5 8 13 21 34 55 89 
+
+
+Another Approach:
+vector<long long> printFibb(int n)
+{
+    // code here
+    vector<long long> v(n);
+    v[0] = v[1] = 1;
+    for (int i = 2; i < n; i++)
+    {
+        v[i] = v[i - 1] + v[i - 2];
+    }
+    return v;
+}
+*/
+
+
 class Solution
 {
 public:
+    // TC : O(n) SC : O(n)
     // Function to return list containing first n fibonacci numbers.
     vector<long long> printFibb(int n)
     {
@@ -28,19 +58,6 @@ public:
     }
 };
 
-/* Another Approach with lower syntax
-
-vector<long long> printFibb(int n)
-{
-    // code here
-    vector<long long> v(n);
-    v[0] = v[1] = 1;
-    for (int i = 2; i < n; i++)
-    {
-        v[i] = v[i - 1] + v[i - 2];
-    }
-    return v;
-}*/
 
 //{ Driver Code Starts.
 int main()
