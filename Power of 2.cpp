b662e31ff732a76861223268a6df9915a01fda94
purushottamnawale <nawalepurushottam@gmail.com>
2023-09-18 23:58:53 +0530

    Power of 2

--- Power of 2.cpp
+++ Power of 2.cpp
@@ -7,9 +7,48 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+/*
+Power of 2
+
+Input:
+1 32
+
+Output:
+1
+
+Explaination:
+2 10
+&&
+1 01
+= 0
+
+4 100
+&&
+3 011
+= 0
+
+8 1000
+&& 
+7 0111
+= 0
+
+16 10000
+&&
+15 01111
+= 0
+
+32 100000
+&&
+31 011111
+= 0
+
+return (n!=0 && (n&(n-1))==0);
+*/
+
 class Solution
 {
 public:
+    // TC: O(1) and SC: O(1)
     // Function to check if given number n is a power of two.
     bool isPowerofTwo(long long n)
     {
