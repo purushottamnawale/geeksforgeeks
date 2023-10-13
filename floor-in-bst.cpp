0e4d97531e1d6b7574cd1c9a1401f0fb5e063d33
purushottamnawale <nawalepurushottam@gmail.com>
2023-10-13 22:48:11 +0530

    Floor in BST

---
+++ floor-in-bst.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to search a node in BST.
+class Solution{
+
+public:
+int ans=-1;
+    int floor(Node* root, int x) {
+        if(!root) return -1;
+        if(root->data==x) return x;
+        if(root->data<x){
+            ans=max(ans,root->data);
+            return ans=max(ans,floor(root->right,x));
+        }
+        else
+        return ans=max(ans,floor(root->left,x));
+  }
+};
+
+//{ Driver Code Starts.
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+        Solution obj;
+        cout << obj.floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
